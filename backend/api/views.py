bf7fac0047292a2972749bf3138f35a1468ccde4
ephrim-s <ephrims5@gmail.com>
2025-03-14 15:53:15 +0000

    updated

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
-
 from api import srializers as api_serializer
 from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
+from userauths.models import User, Profile
+
 
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = api_serializer.MyTokenObtainPairSerializer
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_class = [AllowAny]
+    serializer_class = api_serializer.RegisterSerializer
