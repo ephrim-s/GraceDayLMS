912ce6a0608a4dfe5d70e5ed80505da51c833513
ephrim-s <ephrims5@gmail.com>
2025-03-15 13:18:01 +0000

    updated login authenticatoins

---
+++ frontend/src/utils/auth.js
@@ -0,0 +1,76 @@
+import { useAuthStore } from "../store/auth"
+import axios from "./axios";
+import jwt_decode from "jwt-decode";
+import Cookie from "js-cookie";
+import Swal from "sweetalert2";
+
+export const login = async (email, password) => {
+    try {
+        const {data, status} = await axios.post(`user/token/`, {email, password,});
+
+        if (status === 200) {
+            setAuthUser(data.access, data.refresh);
+            alert("login Successfull");
+        };
+        return {data, error: null};
+
+    } catch (error) {
+        return {
+            data: null,
+            error: error.response.data?.details || "someting went wrong",
+        };
+    }
+};
+
+export const register = async (full_name, email, password, password2) => {
+    try {
+        const {data} = await axios.post(`user/register/`, {full_name, email, password, password2})
+        await login(email, password);
+        alert("Registration Successfull");
+        return {data, error: null}
+    } catch (error) {
+        return {
+            data: null,
+            error: error.response.data?.details || "someting went wrong",
+        };
+    }
+}
+
+export const logout = () => {
+    Cookie.remove("access_token");
+    Cookie.remove("refresh_token");
+    useAuthStore.getState().setAuthUser(null);
+
+    alert("You have been loged out");
+};
+
+export const setUser = async () => {
+    const access_token = Cookie.get("access_token");
+    const refresh_token = Cookie.get("refresh_token");
+
+    if (!access_token || refresh_token) {
+        alert("Tokens does not exists");
+        return
+    }
+
+    if (isAccessTokenExpired(access_token)){
+        const response = getRefreshedToken(refresh_token);
+        setAuthUser(response.access, response.refresh);
+    } else {
+        setAuthUser(access_token, refresh_token);
+    }
+}; 
+
+export const setAuthUser = (access_token, refresh_token) => {
+    Cookie.set('access_token', access_token, {expire: 7, secure: true,});
+    Cookie.set('refresh_token', refresh_token, {expire: 7, secure: true,});
+
+    const user = jwt_decode.jwt_decode(access_token) ?? null;
+
+    if (user) {
+        useAuthStore.getState().setUser(user);
+    } else {
+        setAuthUser.getState().setLoading(false);
+    }
+};
+
